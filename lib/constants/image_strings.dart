69f86c5c607828ab2660e67ef129f5fbd5ca648c
Devansh Purwar <75568315+devansh-purwar@users.noreply.github.com>
2024-04-08 08:50:42 +0530

    welcome screen fix

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -5,6 +5,6 @@ const String tOnBoardingImage1 = 'assets/images/3.jpeg';
 const String tOnBoardingImage2 = 'assets/images/4.jpeg';
 const String tOnBoardingImage3 = 'assets/images/5.jpeg';
 
-const String tWelcomeScreenImage = 'assets/images/5.jpeg';
+const String tWelcomeScreenImage = 'assets/images/cgc.jpeg';
 const String tForgetPasswordImage =
     "assets/images/forget_password/forget-password.png";
