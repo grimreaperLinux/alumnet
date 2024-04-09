16a2573c20d6527475463dfaf8d8024190e7e0f8
Devansh Purwar <75568315+devansh-purwar@users.noreply.github.com>
2024-04-09 22:03:08 +0530

    fix

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -3,7 +3,7 @@ const String tSplashImage = 'assets/images/2.png';
 
 const String tOnBoardingImage1 = 'assets/images/3.jpeg';
 const String tOnBoardingImage2 = 'assets/images/4.jpeg';
-const String tOnBoardingImage3 = 'assets/images/5.jpeg';
+const String tOnBoardingImage3 = 'assets/images/6.jpeg';
 
 const String tWelcomeScreenImage = 'assets/images/cgc.jpeg';
 const String tForgetPasswordImage =
