f9bd47b0beb3fb2ecfe8d026c91e612ced82d569
Chirag Mittal <chirag.mittal@finbox.in>
2024-04-09 23:24:54 +0530

    changed pubspect.yaml from images to Images

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -1,10 +1,10 @@
-const String tSplashScreenTopIcon = 'assets/images/1.jpeg';
-const String tSplashImage = 'assets/images/2.png';
+const String tSplashScreenTopIcon = 'assets/Images/1.jpeg';
+const String tSplashImage = 'assets/Images/2.png';
 
-const String tOnBoardingImage1 = 'assets/images/3.jpeg';
-const String tOnBoardingImage2 = 'assets/images/4.jpeg';
-const String tOnBoardingImage3 = 'assets/images/6.jpeg';
+const String tOnBoardingImage1 = 'assets/Images/3.jpeg';
+const String tOnBoardingImage2 = 'assets/Images/4.jpeg';
+const String tOnBoardingImage3 = 'assets/Images/6.jpeg';
 
-const String tWelcomeScreenImage = 'assets/images/cgc.jpeg';
+const String tWelcomeScreenImage = 'assets/Images/cgc.jpeg';
 const String tForgetPasswordImage =
-    "assets/images/forget_password/forget-password.png";
+    "assets/Images/forget_password/forget-password.png";
