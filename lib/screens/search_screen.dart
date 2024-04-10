00ad34c2dba8e0191a0de633334ae85f07e8e8d6
Decoy101 <00amangpt00@gmail.com>
2024-04-10 15:15:48 +0530

    added navigation to community and discussion on search_screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,8 @@
+import 'package:alumnet/models/community.dart';
+import 'package:alumnet/models/discussion.dart';
 import 'package:alumnet/profile/otheruser_profile.dart';
+import 'package:alumnet/screens/community/discussion_detail_screen.dart';
+import 'package:alumnet/screens/community/discussion_screen.dart';
 import 'package:flutter/material.dart';
 import '../services/typesense_search.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
@@ -44,15 +48,20 @@ class _SearchPageState extends State<SearchPage> {
       },
     };
 
-    Future<Map<String, dynamic>> typesenseSearch(String collection, Map<String, String> searchParams) async {
-      final searchResults = await typesenseClient.collection(collection).documents.search(searchParams);
+    Future<Map<String, dynamic>> typesenseSearch(
+        String collection, Map<String, String> searchParams) async {
+      final searchResults = await typesenseClient
+          .collection(collection)
+          .documents
+          .search(searchParams);
       return searchResults;
     }
 
     try {
       Map<String, dynamic> results = {};
       for (String collection in ['Users', 'Communities', 'Discussions']) {
-        final result = await typesenseSearch(collection, searchParameters[collection] as Map<String, String>);
+        final result = await typesenseSearch(
+            collection, searchParameters[collection] as Map<String, String>);
         results[collection] = result['hits'];
       }
       setState(() {
@@ -96,7 +105,8 @@ class _SearchPageState extends State<SearchPage> {
                 ),
                 filled: true,
                 fillColor: Colors.grey[200],
-                contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: 20),
               ),
               onFieldSubmitted: performSearch,
             ),
@@ -128,15 +138,19 @@ class _SearchPageState extends State<SearchPage> {
                       : ListView(
                           children: [
                             SearchResultBox(
-                              title: 'Users', results: _searchResults['Users'], // Assume this is filtered for users
+                              title: 'Users',
+                              results: _searchResults[
+                                  'Users'], // Assume this is filtered for users
                             ),
                             SearchResultBox(
                               title: 'Communities',
-                              results: _searchResults['Communities'], // Assume this is filtered for communities
+                              results: _searchResults[
+                                  'Communities'], // Assume this is filtered for communities
                             ),
                             SearchResultBox(
                               title: 'Discussions',
-                              results: _searchResults['Discussions'], // Assume this is filtered for discussions
+                              results: _searchResults[
+                                  'Discussions'], // Assume this is filtered for discussions
                             ),
                           ],
                         ),
@@ -182,7 +196,8 @@ class SearchResultBox extends StatelessWidget {
             Divider(),
             ListView.builder(
               shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(), // to disable ListView's scrolling
+              physics:
+                  NeverScrollableScrollPhysics(), // to disable ListView's scrolling
               itemCount: results.isEmpty ? 1 : results.length,
               itemBuilder: (context, index) {
                 if (results.isEmpty) {
@@ -192,21 +207,41 @@ class SearchResultBox extends StatelessWidget {
                   );
                 }
                 final document = results[index]['document'];
+                String userId = document['userId'];
+                Community community = document['name'];
+                Discussion headline = document['headline'];
+                Widget destination;
+                if (userId != null) {
+                  destination = OtherProfile(userId: userId);
+                } else if (community != null) {
+                  destination = DiscussionScreen(community: community);
+                } else if (headline != null) {
+                  destination = DiscussionDetailScreen(
+                      discussion: headline,
+                      community: community,
+                      path:
+                          "discussions/${community.name}/posts/${headline.id}/comments");
+                } else {
+                  destination = Text("NN");
+                }
+
                 return ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                   leading: CircleAvatar(
-                    backgroundImage: NetworkImage(entities_to_img[title] as String),
+                    backgroundImage:
+                        NetworkImage(entities_to_img[title] as String),
                     backgroundColor: Colors.transparent,
                   ),
-                  title: Text(document['fullName'] ?? document['name'] ?? document['headline'] ?? 'No title',
+                  title: Text(
+                      document['fullName'] ??
+                          document['name'] ??
+                          document['headline'] ??
+                          'No title',
                       style: TextStyle(fontWeight: FontWeight.bold)),
                   onTap: () {
                     Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => OtherProfile(
-                          userId: document['id'],
-                        ),
-                      ),
+                      MaterialPageRoute(builder: (context) => destination),
                     );
                     print(document['id']);
                   },
